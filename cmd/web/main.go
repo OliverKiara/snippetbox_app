fa3cd14fcbff4f7ce37d14eb313d4f628b266b61
oliver <oliverkiara99@gmail.com>
2019-09-28 13:57:16 +0300

    initial commit cached templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,23 +1,25 @@
 package main
 
-import(
+import (
 	"database/sql"
 	"flag"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+
 	"alexedwards.net/snippetbox/pkg/models/mysql"
 	_ "github.com/go-sql-driver/mysql"
-	
 )
-type application struct{
-	errorLog *log.Logger
-	infoLog *log.Logger
-	snippets *mysql.SnippetModel
-	
+
+type application struct {
+	errorLog      *log.Logger
+	infoLog       *log.Logger
+	snippets      *mysql.SnippetModel
+	templateCache map[string]*template.Template
 }
 
-func main(){
+func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP Network address")
 	flag.Parse()
@@ -28,47 +30,43 @@ func main(){
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	
-	 db, err := openDB(*dsn)
-	 if err != nil{
-		 errorLog.Fatal(err)
-		 return
-	 }
-	 defer db.Close()
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+		return
+	}
+	defer db.Close()
 
-	 
-	 templateCache, err := newTemplateCache("./ui/html/")
-	     if err != nil {
-		 errorLog.Fatal(err)
-	 
+	templateCache, err := newTemplateCache("./ui/html/")
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 
 	app := &application{
-		errorLog: errorLog,
-		infoLog: infoLog,
-		snippets: &mysql.SnippetModel{DB: db},
-		
+		errorLog:      errorLog,
+		infoLog:       infoLog,
+		snippets:      &mysql.SnippetModel{DB: db},
+		templateCache: templateCache,
 	}
 
 	srv := &http.Server{
-		Addr: *addr,
+		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler: app.routes(),
+		Handler:  app.routes(),
 	}
 
-	
 	infoLog.Printf("starting server on port %s", *addr)
 	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 
 }
- func openDB(dsn string) (*sql.DB, error){
+func openDB(dsn string) (*sql.DB, error) {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	if err := db.Ping(); err != nil{
+	if err := db.Ping(); err != nil {
 		return nil, err
 	}
 	return db, nil
 }
-
