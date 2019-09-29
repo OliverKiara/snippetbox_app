e0bbd3c6a704ee68430ec7a00d46c1d4f2a3334c
oliver <oliverkiara99@gmail.com>
2019-09-29 13:25:53 +0300

    added create form page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,10 +10,7 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
+	
 	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -24,8 +21,26 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	})
 
 }
+
+func (app *application)createSnippetForm(w http.ResponseWriter, r *http.Request){
+	app.render(w, r, "create.page.tmpl", nil)
+}
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+	
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
+}
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -42,22 +57,4 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		Snippet: s,
 	})
 
-}
-func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Header().Set("allow", "POST")
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := "7"
-
-	id, err := app.snippets.Insert(title, content, expires)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
